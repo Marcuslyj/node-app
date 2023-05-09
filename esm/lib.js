6848ccb19d98c794eaafaee7f552942e8789722e
Marcus <814712074@qq.com>
2023-05-09 14:33:30 +0800

    esm & build

---
+++ esm/lib.js
@@ -0,0 +1,3 @@
+export const getSum = (num1, num2) => num1 + num2;
+
+export const getMultiple = (num1, num2) => num1 * num2;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log(process.argv)
+
+import { getSum } from "./esm/lib.js"
+
+console.log(getSum(1, 2))
