1c9a7914d3072f6a41a211428a02e910d17bf1d1
Marcus <814712074@qq.com>
2023-05-09 14:39:54 +0800

    输入监听

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.log(process.argv)
 import { getSum } from "./esm/lib.js"
 
 console.log(getSum(1, 2))
+
+
+// 监听输入
+process.stdin.on('data', e=>{
+  const action = e.toString().trim()
+  console.log(action)
+  if (action === 'kill') {
+    process.exit()
+  }
+})
